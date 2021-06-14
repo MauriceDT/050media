e6706a8d36e191a2ae30f7ed062e577466a6530b
Maurice di Tolve <maurice_di_tolve@hotmail.com>
2021-06-14 14:41:43 +0200

    14/06/2021 het is warm

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>050Media Front End</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- BootStrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" integrity="undefined" crossorigin="anonymous">
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+<body>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="topnav">
+    <div class="topnav-left">
+        <img class="logo-acme" src="/images/logo_ACME.png" alt="">
+    </div>
+    <div class="topnav-right">
+        <a href="#">Menu item #1</a>
+        <a href="#">Menu item #2</a>
+        <a href="#">Menu item #3</a>
+        <a href="#">Menu item #4</a>
+        <a class="modal-btn" id="shoppingcart">Winkelwagen</a>
+    </div>
+</nav>
